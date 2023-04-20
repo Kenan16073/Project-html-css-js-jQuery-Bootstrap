102f11e78c8e9aec926f4adfc59d5d6fa65ac25b
Kenan <kananmh@code.edu.az>
2023-04-21 00:28:36 +0400

    front

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,59 @@
 'use strict'
+
+
+$(function () {
+
+    $('.cards').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    window.onload=function(){
+        $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:1500,
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev"></button>',
+        nextArrow:'<button type="button" class="slick-next"></button>',
+        centerMode:true,
+        slidesToShow:3,
+        slidesToScroll:1
+        });
+
+
+        $('.slider2').slick({
+            autoplay:true,
+            autoplaySpeed:1500,
+            arrows:true,
+            prevArrow:'<button type="button" class="slick-prev"></button>',
+            nextArrow:'<button type="button" class="slick-next"></button>',
+            centerMode:true,
+            slidesToShow:6,
+            slidesToScroll:1
+            });
+      };
+      
+});
+
