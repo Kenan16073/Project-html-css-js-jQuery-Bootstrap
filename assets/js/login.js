bbeaea27f2fa5db7c882bf4b20072c9c646a0920
Kenan <kananmh@code.edu.az>
2023-04-24 02:43:04 +0400

    front

---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+const togglePassword = document.querySelector('#password-inp');
+  const password = document.querySelector('#eye');
+
+  togglePassword.addEventListener('click', function (e) {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
