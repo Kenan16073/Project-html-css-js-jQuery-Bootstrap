80b69bd01affc6e28c9dcf22780bc3e9fd0cdba7
Kenan <kananmh@code.edu.az>
2023-04-25 22:28:59 +0400

    front

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,16 +1,30 @@
 const togglePassword = document.querySelector('#password-inp');
   const password = document.querySelector('#eye');
 
-  password.addEventListener('click', function (e) {
+  password.addEventListener('click', function () {
     let type = togglePassword.getAttribute('type');
     if(type === 'password'){
       type = 'text';
-      this.className = 'fa-solid fa-eye-slash fa-beat';
+      this.className = 'fa-solid fa-eye-slash ';
     }else {
       type = 'password';
-      this.className = 'fa-solid fa-eye fa-beat';
+      this.className = 'fa-solid fa-eye ';
     }
     togglePassword.setAttribute('type', type);
+});
+const togglePassword2 = document.querySelector('#password-inp2');
+  const password2 = document.querySelector('#eye2');
+
+  password2.addEventListener('click', function () {
+    let type = togglePassword2.getAttribute('type');
+    if(type === 'password'){
+      type = 'text';
+      this.className = 'fa-solid fa-eye-slash ';
+    }else {
+      type = 'password';
+      this.className = 'fa-solid fa-eye ';
+    }
+    togglePassword2.setAttribute('type', type);
 });
 
 function openCity(evt, cityName) {
